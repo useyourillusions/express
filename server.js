ee2367efa9b83ad1be24a58ceea95b2a3aa09100
useyourillusions <minsk.bay@gmail.com>
2020-07-19 22:21:53 +0300

    next

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-// app.get('/', (req, res) => {
-//     res.end('.')
-// });
+app.get('/test', (req, res) => {
+    res.end('.')
+});
 
 // app.listen(PORT, (...arg) => {
 //     console.log(arg);
 // })
 
 // app.use(express.static('./dist/heroku-test'));
-app.use(express.static(path.join(__dirname, '/dist/heroku-test')));
+app.use(express.static(path.join(__dirname, 'dist/heroku-test')));
 
 // app.get('/*', function (req, res) {
 //   res.sendFile('index.html', { root: 'dist/heroku-test' });
 // });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/heroku-test/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/heroku-test/index.html'));
 });
 
 app.listen(process.env.PORT || PORT);
