362f1bbe8277c13e309a18305cc8fe45a4da9b9c
useyourillusions <minsk.bay@gmail.com>
2020-07-19 21:50:24 +0300

    next

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/heroku-test/index.html'));
 });
 
 app.listen(process.env.PORT || PORT);
