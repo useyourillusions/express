d62ba4e1d9c19006325300526120cf47aad2a420
useyourillusions <minsk.bay@gmail.com>
2020-08-06 22:52:43 +0300

    Added dist path

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const app = express();
 const PORT = 5000;
 const isProd = process.argv.includes('--prod');
 const dbDir = './BACKEND/DB';
+const { projects } = require('./angular.json');
+const distPath = projects['heroku-test'].architect.build.options.outputPath;
 
 const signUpHandlerPost = require('./BACKEND/api-routes/sign-up');
 const signInHandlerPost = require('./BACKEND/api-routes/sign-in');
-
 const userHandlerGet = require('./BACKEND/api-routes/user');
-
 const eventsHandlerGet = require('./BACKEND/api-routes/events/get');
 const eventsHandlerPost = require('./BACKEND/api-routes/events/post');
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'dist/heroku-test')));
+app.use(express.static(path.join(__dirname, distPath)));
 
 // app.get('/*', function (req, res) {
 //     res.sendFile('index.html', { root: 'dist/heroku-test' });
 // });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/heroku-test/index.html'));
+    res.sendFile(path.join(__dirname, distPath + '/index.html'));
 });
 
 // Auth routes
